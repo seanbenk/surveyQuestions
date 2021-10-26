86b4b62b1c1261271f2d4b54954b46ac5deaae43
seanbenkenstein <seanbenkenstein@gmail.com>
2021-10-27 00:50:53 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const rows = [
+  ["name1", "city1", "some other info"],
+  ["name2", "city2", "more info"],
+];
+
+const quiz = document.getElementById("quiz");
+
+quiz.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const questionArr = ["Question Number"];
+  const answerArr = ["Answer"];
+  const inputs = document.querySelectorAll("input");
+  // e.target.forEach((input) => {
+  //   console.log(input);
+  // });
+  inputs.forEach((input) => {
+    if (!input.checked) return;
+    questionArr.push(input.name);
+    answerArr.push(input.value);
+  });
+
+  const csvContent =
+    "data:text/csv;charset=utf-8," +
+    [questionArr, answerArr].map((e) => e.join(",")).join("\n");
+
+  const encodedUri = encodeURI(csvContent);
+  window.open(encodedUri);
+});
